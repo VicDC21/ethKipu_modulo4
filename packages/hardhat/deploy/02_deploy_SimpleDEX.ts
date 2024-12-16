1484568dcee1b5a033e2ca9689d24a6ad61c4745
Victor Cipriano <vcipriano@fi.uba.ar>
2024-12-15 23:33:36 -0300

    unused import

--- packages/hardhat/deploy/02_deploy_SimpleDEX.ts
+++ packages/hardhat/deploy/02_deploy_SimpleDEX.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
 import { deployments } from "hardhat";
 
 /**
